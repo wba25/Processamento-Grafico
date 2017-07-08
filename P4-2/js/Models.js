82c291ccf0fa26ea54dacd924ab61a891fa82177
hitallocavas <hcs3@cin.ufpe.br>
2017-07-08 14:25:54 -0300

    Norma do vetor

--- P4-2/js/Models.js
+++ P4-2/js/Models.js
@@ -7,3 +7,11 @@ function Vetor(modulo, direcao){
     this.modulo = modulo;
     this.direcao = direcao;
 }
+
+function Norma(a){
+  sum = 0;
+  for(i = 0; i<a.lenght; i++){
+    sum = a[i]*a[i] + sum;
+  }
+  return Math.sqrt(sum);
+}

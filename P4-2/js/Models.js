a38512e90f755078a0222100c6f5004205104971
Wellington Almeida <wba@cin.ufpe.br>
2017-07-12 14:48:36 -0300

    adiciona calculo de angulo entre vetores e testa produto interno e produto vetorial

--- P4-2/js/Models.js
+++ P4-2/js/Models.js
@@ -15,17 +15,33 @@ function Vetor3D(x,y,z){
     this.z = z;
 }
 
+// Calcula valor da norma do vetor
 Vetor3D.prototype.norma = function(){
     return Math.sqrt(Math.pow(this.x,2)+Math.pow(this.y,2)+Math.pow(this.z,2));
 };
 
-Vetor3D.prototype.produtoEscalar = function(v){
+// Calcula produto interno
+Vetor3D.prototype.produtoInterno = function(v){
     return (this.x*v.x + this.y*v.y + this.z*v.z);
 };
 
+// Calcula vetor resultado do produto vetorial
 Vetor3D.prototype.produtoVetorial = function(v){
-    var x = this.y*v.z - this.z*v.y;
-    var y = this.z*v.x - this.x*v.z;
-    var z = this.x*v.y - this.y*v.x;
-    return new Vetor(x, y, z);
+    var i = this.y*v.z - this.z*v.y;
+    var j = this.z*v.x - this.x*v.z;
+    var k = this.x*v.y - this.y*v.x;
+    return new Vetor3D(i, j, k);
 };
+
+// Valor do cosseno de 2 vetores
+Vetor3D.prototype.cosAngulo = function(v){
+    console.log(this.produtoInterno(v));
+    console.log(this.norma());
+    console.log(v.norma());
+    return this.produtoInterno(v) / (this.norma() * v.norma());
+} 
+
+// Ângulo entre 2 vetores em graus
+Vetor3D.prototype.angulo = function(v){
+    return Math.acos(this.cosAngulo(v))*(180/Math.PI);
+} 

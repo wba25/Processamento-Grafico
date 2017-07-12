91482772029a92f02dbd62502279b95fa55ba17c
Wellington Almeida <wba@cin.ufpe.br>
2017-07-12 11:04:00 -0300

    Conserta e testa Calculo de Norma de vetor

--- P4-2/js/Models.js
+++ P4-2/js/Models.js
@@ -9,36 +9,23 @@ function Ponto3D(x,y,z){
   this.z = z;
 }
 
-function Vetor(x,y,z){
+function Vetor3D(x,y,z){
     this.x = x;
     this.y = y;
     this.z = z;
+}
 
+Vetor3D.prototype.norma = function(){
+    return Math.sqrt(Math.pow(this.x,2)+Math.pow(this.y,2)+Math.pow(this.z,2));
+};
 
-    Norma = function(){
-    sum = 0;
-    a = [this.x,this.y,this.z];
+Vetor3D.prototype.produtoEscalar = function(v){
+    return (this.x*v.x + this.y*v.y + this.z*v.z);
+};
 
-    for(i = 0; i<a.lenght; i++){
-      sum = a[i]*a[i] + sum;
-    }
-
-    norma = Math.sqrt(sum);
-
-    thix.x = this.x/norma;
-    thix.y = this.y/norma;
-    thix.z = this.z/norma;
-    }
-
-    produtoEscalar = function(k){
-      return (this.x*v.x + this.y*v.y + this.z*v.z);
-    }
-
-    produtoVetorial =  function(v){
-      var x = this.y*v.z - this.z*v.y;
-      var y = this.z*v.x - this.x*v.z;
-      var z = this.x*v.y - this.y*v.x;
-      return new Vetor(x, y, z);
-    }
-
-  }
+Vetor3D.prototype.produtoVetorial = function(v){
+    var x = this.y*v.z - this.z*v.y;
+    var y = this.z*v.x - this.x*v.z;
+    var z = this.x*v.y - this.y*v.x;
+    return new Vetor(x, y, z);
+};

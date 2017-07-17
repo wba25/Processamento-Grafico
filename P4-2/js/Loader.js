f5d47f75f794cc5f31f2fa3e357c069d2dd6ea0a
hitallocavas <hcs3@cin.ufpe.br>
2017-07-16 22:26:26 -0300

    Carregamento da Iluminação

--- P4-2/js/Loader.js
+++ P4-2/js/Loader.js
@@ -7,7 +7,52 @@ alert(data);
 }
 
 function loadIluminacao(data) {
-alert(data);
+  // Se não tiver objeto, vai iluminar o que? o vazio, então pode receber objetos
+  // de entrada
+  if(!objeto) document.getElementById('objeto').disabled = false;
+  iluminacao = {};
+
+  //aqui eu declaro uma variável controladora das entradas
+
+  var a;
+  //usando split, eu crio um vetor da entrada, por exemplo,
+  //aqui eu tou controlando por espaços
+  //se a primeira linha tiver '22 04 6', vai virar um vetor a(22,4,6)
+
+  a = data[0].split(' ');
+  //aqui eu pego a posição da fonte em coordenadas de mundo
+  var pl = new Ponto3D(a[0], a[1], a[2]);
+  // aqui eu pego o coeficiente de reflexão ambiental
+  var ka = data[1];
+
+  // aqui eu pego o vetor de cor do ambiente
+  var a = data[2].split(' ');
+  var ia = new Vetor(a[0], a[1], a[2]);
+  // aqui eu pego o coeficiente difuso
+  var kd = data[3];
+
+  // aqui eu pego o vetor difuso
+  var a = data[4].split(' ');
+  var od = new Vetor(a[0], a[1], a[2]);
+
+  // aqui eu pego o coeficiente especular, para onde vai ser refletida a luz
+  var ks = data[5];
+
+  // aqui eu pego a cor da fonte de luz
+  var a = data[6].split(' ');
+  var il = new Vetor(a[0], a[1], a[2]);
+
+  // aqui eu pego o coeficiente de rugosidade
+  var n = data[7];
+
+  // crio um novo objeto, onde suas especificações estão no arquivo Models.js
+  iluminacao = new Iluminacao(pl, ka, ia, kd, od, ks, il, n);
+
+  // aqui eu faço a adaptação de acordo com o ponto de vista da camera
+  iluminacao.pl = iluminacao.pl_pvista_camera.camera_pontoDeVista(camera);
+
+  //NÃO RECEBE MAIS OBJETOS, TÁ BOM, ISSO AKI NÃO É CABARÉ, CARAMBA
+  document.getElementById('objeto').disabled = false;
 }
 
 function loadObjeto(data) {
--- P4-2/js/Models.js
+++ P4-2/js/Models.js
@@ -9,6 +9,20 @@ function Ponto3D(x,y,z){
   this.z = z;
 }
 
+//dada a camera, o ponto de vista do observador, sacou?
+//é como se por exemplo, você tirar uma foto de lado ou de frente
+// se for de lado, o ponto de vista é geometricamente de lado
+//caso contrário, vai ser de frente kkkkk
+// exemplo bosta mas vai dá pra entender.
+
+Ponto3D.prototype.camera_pontoDeVista = function(camera) {
+  var a = this.clone();
+  var b = a.sub(camera.c);
+  var r = b.multiplicarMatrix(camera.alfa);
+  return r;
+};
+
+
 function Vetor3D(x,y,z){
     this.x = x;
     this.y = y;
@@ -45,10 +59,33 @@ Vetor3D.prototype.cosAngulo = function(v){
     console.log(this.norma());
     console.log(v.norma());
     return this.produtoInterno(v) / (this.norma() * v.norma());
-} 
+}
 
 // Ângulo entre 2 vetores em graus
 Vetor3D.prototype.angulo = function(v){
     return Math.acos(this.cosAngulo(v))*(180/Math.PI);
 }
 
+
+//pl - coordenadas originais
+//ka - reflexao ambiental
+//Ia - vetor cor ambiental
+//kd - constante difusa
+//od - vetor difuso
+//ks - parte especular
+//Il - cor da fonte de luz
+//n  - constante de rugosidade
+
+function Iluminacao(pl, ka, ia, kd, od, ks, il, n) {
+  this.pl = pl; //posições originais da fonte de luz em coordenadas de mundo
+  this.pl_pvista_camera = pl; //adapta as posiçoes de entrada para o ponto de vista da camera
+  this.ka = ka; // reflexão de luz ambiente (quanto maior, mais reflexível é)
+  this.ia = ia; // intensidade da luz ambiente (quanto maior, mais iluminado kkk)
+  this.kd = kd; // constante difusa, o quão espalhável é essa luz
+  this.od = od; // vetor difuso, usado para simplesmente espalhar kkk
+  this.ks = ks; // parte especular, como a luz vai ser refletida no objeto
+  this.il = il; // cor da fonte de luz
+  this.n = n; // constante de rugosidade, trata irregularidades, ruídos, enfim, frescura.
+}
+
+//DEPOIS VOU TRATAR A COLORAÇÃO DO OBJETO DE ACORDO COM A ILUMINAÇÃO, PERAÍ
